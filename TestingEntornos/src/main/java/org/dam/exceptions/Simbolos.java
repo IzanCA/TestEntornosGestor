d014d8ba8b4e7acb8ba696514864104728525d57
IzanCA <153659311+IzanCA@users.noreply.github.com>
2024-02-22 15:48:32 +0100

    Add original project
    añadimos el proyecto solo con las excepciones

---
+++ TestingEntornos/src/main/java/org/dam/exceptions/Simbolos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.dam.exceptions;
+
+/**
+ *
+ * @author pgrau
+ */
+public class Simbolos extends Exception{
+
+    public Simbolos(String message) {
+        super(message);
+    }
+    
+}
